9ff7fb34a9a18b575492927deb4cd13ed2485898
syeal01 <syeal01@hotmail.com>
2021-06-25 13:53:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Coded by: Aliabbas Syed
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Pushed 1");
+            stack.Push("Pushed 2");
+            stack.Push("Pushed 3");
+
+            foreach (var item in stack)
+                Console.WriteLine(item);
+
+            Console.WriteLine("----------");
+
+            try
+            {
+                stack.Pop();
+                stack.Pop();
+                stack.Pop();
+
+                // Add another Pop() to catch exception
+                // stack.Pop();
+
+                foreach (var item in stack)
+                    Console.WriteLine(item);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            finally
+            {
+                Console.WriteLine("stack done");
+            }
+        }
+    }
+}
